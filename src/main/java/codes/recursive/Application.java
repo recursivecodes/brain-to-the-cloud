1ef4d9887791e0d86796d3ccbbbd231e1812e1b6
recursivecodes <toddraymondsharp@gmail.com>
2021-12-03 10:00:37 -0500

    fix acme module conflict with ATP

--- src/main/java/codes/recursive/Application.java
+++ src/main/java/codes/recursive/Application.java
@@ -1,10 +1,16 @@
 package codes.recursive;
 
 import io.micronaut.runtime.Micronaut;
+import oracle.security.pki.OraclePKIProvider;
+
+import java.security.Security;
 
 public class Application {
 
     public static void main(String[] args) {
+        if (null == Security.getProvider("OraclePKI")) {
+            Security.insertProviderAt(new OraclePKIProvider(), Integer.MAX_VALUE);
+        }
         Micronaut.run(Application.class, args);
     }
 }
