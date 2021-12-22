45fd9a9df25c4870a02cfd1ddcaca323f9b3ed5f
recursivecodes <toddraymondsharp@gmail.com>
2021-12-22 14:34:41 -0500

    clean up http client

--- src/main/java/codes/recursive/config/CodClientConfig.java
+++ src/main/java/codes/recursive/config/CodClientConfig.java
@@ -9,6 +9,24 @@ import io.micronaut.core.annotation.Nullable;
 public class CodClientConfig {
     @Nullable
     private String ssoToken;
+    private String userAgent;
+    private String xsrfToken;
+
+    public String getXsrfToken() {
+        return xsrfToken;
+    }
+
+    public void setXsrfToken(String xsrfToken) {
+        this.xsrfToken = xsrfToken;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+    }
 
     public String getSsoToken() {
         return ssoToken;
